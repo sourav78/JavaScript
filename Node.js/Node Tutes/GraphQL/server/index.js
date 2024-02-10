ddd9f83abbc9a3b6930b9a46bb1eb95e4664ff06
sourav78 <souravranjan488@gmail.com>
2024-02-10 09:14:32 +0530

    GraphQL Initiated

---
+++ Node.js/Node Tutes/GraphQL/server/index.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const { ApolloServer } = require("@apollo/server")
+const { expressMiddleware } = require("@apollo/server/express4")
+const bodyParser = require('body-parser')
+const cors = require('cors')
