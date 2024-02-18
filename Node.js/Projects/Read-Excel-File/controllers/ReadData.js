1083cde6b31199db8ecd7c8d1f25827ea2d96bc9
sourav78 <souravranjan488@gmail.com>
2024-02-18 10:17:53 +0530

    Read data form excel - Raed

---
+++ Node.js/Projects/Read-Excel-File/controllers/ReadData.js
@@ -0,0 +1,18 @@
+import xlsx from 'xlsx'
+import path from 'path'
+
+const readDataFromExcel = (req, res) => {
+    
+    const excelSheet = xlsx.readFile('assets/bgmi.xlsx')
+    let workbook_sheet = excelSheet.SheetNames
+
+    let excelData = xlsx.utils.sheet_to_json(
+        excelSheet.Sheets[workbook_sheet[0]]
+    )
+
+    res.status(200).json({
+        data: excelData
+    })
+}
+
+export {readDataFromExcel}
--- Node.js/Projects/Read-Excel-File/index.js
+++ Node.js/Projects/Read-Excel-File/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { readDataFromExcel } from "./controllers/ReadData.js";
 
 const app = express()
 
@@ -6,4 +7,7 @@ app.get("/", (re, res) => {
     res.send("Hello XLXS")
 })
 
+app.get("/read-data", readDataFromExcel)
+
 app.listen(3002, () => console.log(`Server listen on port: 3002`))
+// Read data form excel initialized
