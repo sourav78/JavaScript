d4486cea8639812169da4e75419bc69f5e211715
sourav78 <souravranjan488@gmail.com>
2025-10-05 13:08:02 +0530

    interview  - map filter reduce questions

--- Interview/MapFilterReduce/mapFilterReduce.js
+++ Interview/MapFilterReduce/mapFilterReduce.js
@@ -96,20 +96,117 @@ const numArr = [1, 3, 4, 6, 7, 2]
 // console.log(multipleOfEachElement);
 
 
-let students = [
-    {
-        name: "Piyush",rollNumber: 31, marks: 80
-    }, {
-        name: "Jenny", rollNumber: 15, marks: 69
-    }, {
-        name: "Kaushal", rollNumber: 16, marks: 35
-    }, {
-        name : "Dilpreet" , rollNumber: 7, marks: 55
+// let students = [
+//     {
+//         name: "Piyush",rollNumber: 31, marks: 80
+//     }, {
+//         name: "Jenny", rollNumber: 15, marks: 69
+//     }, {
+//         name: "Kaushal", rollNumber: 16, marks: 35
+//     }, {
+//         name : "Dilpreet" , rollNumber: 7, marks: 55
+//     }
+// ]
+
+// const result = students.map((student) => student.marks < 60 ? student.marks + 20 : student.marks)
+//                         .filter(marks => marks > 60)
+//                         .reduce((acc, marks) => acc+= marks, 0)
+
+// console.log(result);
+
+
+
+//q1. Given an array of objects users, print fullname.
+
+const users=[
+  {firstName:"john",lastName:"Biden",age:26},
+  {firstName:"jimmy",lastName:"cob",age:75},
+  {firstName:"sam",lastName:"lewis",age:50},
+  {firstName:"Ronald",lastName:"Mathew",age:26},  
+];
+
+let fullUser = users.map((user) => user.firstName+" "+user.lastName)
+
+console.log(fullUser);
+
+// Q2. Given an array of objects users, print user of particular age along with their frequency.
+
+const users1=[
+  {firstName:"john",lastName:"Biden",age:26},
+  {firstName:"jimmy",lastName:"cob",age:75},
+  {firstName:"sam",lastName:"lewis",age:50},
+  {firstName:"Ronald",lastName:"Mathew",age:26},  
+];
+
+let agedUser = users1.reduce((acc, user) => {
+    if(acc[user.age]){
+        acc[user.age] += 1
+    }else{
+        acc[user.age] = 1
     }
-]
 
-const result = students.map((student) => student.marks < 60 ? student.marks + 20 : student.marks)
-                        .filter(marks => marks > 60)
-                        .reduce((acc, marks) => acc+= marks, 0)
+    return acc
+}, {})
 
-console.log(result);
+console.log(agedUser);
+
+//Q3.Print object having marks greater than 60 and rollNumber greater than 15.
+
+let student =[
+ {name:"Smith",rollNumber:31,marks:80},
+ {name:"Jenny",rollNumber:15,marks:69},
+ {name:"John",rollNumber:16,marks:35},
+ {name:"Tiger",rollNumber:7,marks:55}
+];
+
+student.forEach(stud => stud.rollNumber > 15 && stud.marks > 60 && console.log(stud))
+
+
+//Q4.Print sum of marks of all the student
+
+let student1 =[
+ {name:"Smith",rollNumber:31,marks:80},
+ {name:"Jenny",rollNumber:15,marks:69},
+ {name:"John",rollNumber:16,marks:35},
+ {name:"Tiger",rollNumber:7,marks:55}
+];
+
+let TotalMarks = student1.reduce((total, stud) => {
+    total += stud.marks
+
+    return total
+}, 0)
+
+console.log(TotalMarks);
+
+
+
+//Q5.List of all firstName from array whose age is more than 30
+
+const users2=[
+  {firstName:"john",lastName:"Biden",age:26},
+  {firstName:"jimmy",lastName:"Cob",age:75},
+  {firstName:"Sam",lastName:"Lewis",age:50},
+  {firstName:"Ronald",lastName:"Mathew",age:26},  
+];
+
+let userArr = users2.filter(user => user.age > 30).map(filterdUser => filterdUser.firstName)
+
+console.log(userArr);
+
+
+// Print total marks for students with marks greater than 60 after 20 marks have been added to those who scored less than 60
+
+let student2 =[
+ {name:"Smith",rollNumber:31,marks:80},
+ {name:"Jenny",rollNumber:15,marks:69},
+ {name:"John",rollNumber:16,marks:35},
+ {name:"Tiger",rollNumber:7,marks:55}
+];
+
+let totalMarks = student2
+                    .map(stud => stud.marks < 60 ? {...stud, marks: stud.marks+20} : stud)
+                    .filter(stud => stud.marks > 60)
+                    .reduce((total, stud) => total += stud.marks, 0)
+
+console.log(totalMarks);
