58d68ccab76a97a2c77c12a8c24dda1cddd6c2c6
sourav78 <souravranjan488@gmail.com>
2025-10-05 12:32:21 +0530

    interview - promise race and any polyfills

--- Interview/promise/PromisePolyfills.js
+++ Interview/promise/PromisePolyfills.js
@@ -50,6 +50,48 @@ function customPromiseAllSettled(promiseArr) {
   })
 }
 
+// Polifill of Promise.any()
+
+function customPromiseAny(promiseArr) {
+  return new Promise((resolve, reject) => {
+
+    let rejectArr = []
+
+    for (let i = 0; i < promiseArr.length; i++) {
+      promiseArr[i].then(data => {
+        resolve(data)
+        return
+      }).catch(err => {
+        rejectArr.push(err)
+        if (rejectArr.length === promiseArr.length) {
+          reject(new AggregateError(rejectArr))
+        }
+        // console.log(rejectArr);
+      })
+
+    }
+
+  })
+}
+
+// Polifils of Promise.race()
+
+function customPromiseRace(promiseArr) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promiseArr.length; i++) {
+      // Promise.resolve(promiseArr[i])
+      //   .then(resolve)   // as soon as one resolves → resolve outer promise
+      //   .catch(reject);  // as soon as one rejects → reject outer promise
+
+      promiseArr[i].then(data => {
+        resolve(data)
+      }).catch(err => {
+        reject(err)
+      })
+    }
+  })
+}
+
 
 // This is fucntion is used to create promise
 
@@ -65,12 +107,12 @@ function createAsyncTask(data, time, needToReject) {
   });
 }
 
-const p1 = createAsyncTask("p1", 400);
+const p1 = createAsyncTask("p1", 400, 1);
 const p2 = createAsyncTask("p2", 1000, 1);
-const p3 = createAsyncTask("p3", 500);
-const p4 = createAsyncTask("p4", 3000);
+const p3 = createAsyncTask("p3", 200, 1);
+const p4 = createAsyncTask("p4", 3000, 1);
 
-customPromiseAllSettled([p1, p3, p2, p4]).then(data => {
+customPromiseRace([p1, p3, p2, p4]).then(data => {
   console.log(data);
 }).catch(err => {
   console.log(err);
